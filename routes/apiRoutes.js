d7b71bddb771b774f85b95c8a3e91ae88be56b74
CarolHGray <fivegrays@yahoo.com>
2024-02-01 15:05:02 -0500

    started apiRoutes set-up

---
+++ routes/apiRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const store = require('../db/store');
+
+// Routes request and response commands
+router.get('/notes', (req, res) => {
+    store
+        .getNotes()
+        .then(notes => {
+            res.json(notes)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+// Function route 
+router.post('/notes', (req, res) => {
+    console.log(req.body)
+    store
+        .addNote(req.body)
+        .then(note => {
+            res.json(note)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
