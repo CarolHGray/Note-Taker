aeeef17b0e2bce80cf4fcf9db061239f0aa8bb0f
CarolHGray <fivegrays@yahoo.com>
2024-02-01 15:07:02 -0500

    added function route for deleting notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -25,3 +25,11 @@ router.post('/notes', (req, res) => {
             res.status(500).json(err)
         })
 })
+router.delete('/notes/:id', (req, res) => {
+    store
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err))
+})
+
+module.exports = router;
